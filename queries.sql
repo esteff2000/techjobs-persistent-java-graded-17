c61d78465082bef17461fbdd08da3c0e9a85fc96
esteff2000 <steffensoner@gmail.com>
2024-06-16 01:14:12 -0500

    Finished task 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,11 @@
 --Part 1
-
+-- id: int
+-- employer: varchar(255)
+-- name : varchar(255)
+-- skills: varchar(255)
 --Part 2
-
+-- select name from employer where location = "St. Louis City";
 --Part 3
-
+-- drop table job;
 --Part 4
+-- select * from skill inner join job_skills on job_skills.skills_id = skill.id where job_skills.jobs_id is not null order by name asc;
